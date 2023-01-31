8a1654c1daa61484c4e3aa13b17bf89792a1d741
Phillip Baird <phillip.baird@gmail.com>
2023-01-31 20:45:28 +1300

    Fix css.

--- src/app.rs
+++ src/app.rs
@@ -22,7 +22,7 @@ pub fn App(cx: Scope) -> impl IntoView {
         <div class="flex flex-col bg-green-300 py-32 space-y-8">
             <h1 class="text-3xl text-center">"Hi from your Leptos app with Tailwind!"</h1>
             <button
-              class="px-3 py-2 mx-auto border border-black bg-white hover:bg-yellow-200 focus:bg-yellow-200 rounded"
+              class="px-3 py-2 mx-auto border border-black bg-white hover:text-white hover:bg-slate-700 rounded"
               on:click=move |_| hello_world.dispatch(())
             >
               "Print Hello on the server"
