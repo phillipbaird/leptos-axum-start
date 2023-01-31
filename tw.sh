352425e1fa2ab5c9e9a5d068c76640b600928f3d
Phillip Baird <phillip.baird@gmail.com>
2023-01-31 15:39:30 +1300

    Fix Tailwind script.

--- tw.sh
+++ tw.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 case "$1" in
     watch)
-      tailwindcss -i ./tailwind.css -o ./assets/main.css --watch
+      tailwindcss -i ./tailwind.css -o ./css/main.css --watch
       ;;
     build)
-      tailwindcss --minify -i ./tailwind.css -o ./assets/main.css
+      tailwindcss --minify -i ./tailwind.css -o ./css/main.css
       ;;
     *)
       echo "Usage: ./tw.sh [OPTION]"
